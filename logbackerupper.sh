07c6a423e35e3452f80fa6addda463b2df62a49e
Jayteaare <94884280+Jayteaare@users.noreply.github.com>
2022-03-12 21:39:46 -0500

    Update logbackerupper.sh

--- logbackerupper.sh
+++ logbackerupper.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-mkdir /var/backups/logbackerupper/
-mkdir /var/backups/logbackerupper/logs/
-mkdir /var/backups/logbackerupper/error/
+if [ -d /var/backups/logbackerupper ]
+then
+        echo "We're just spinning up.."
+        sleep 2
+else
+        echo "It's your first time! Let us take care of some admin stuff!"
+        mkdir /var/backups/logbackerupper/
+        mkdir /var/backups/logbackerupper/error/
+        mkdir /var/backups/logbackerupper/logs/
+        sleep 2
+fi
 
 exec 2>> //var/backups/logbackerupper/error/error.log
 
@@ -14,8 +22,8 @@ sleep 3
 echo "MD5 hashes of files within $USERDIR" >> hashed_logs.txt
 for file in $(find $USERDIR -type f)
 do
-        cp "$file" "/var/backups/logbackerupper/logs/${file##*/}-$(date +"%m-%d-%y-%r")"
-        md5sum $file >> /var/backups/logbackerupper/hashed_logs.txt
+        ls /var/log | grep .log* | cp $file "/var/backups/logbackerupper/logs/${file##*/}-$(date +"%m-%d-%y-%r")"
+        ls /var/log | grep .log* | md5sum $file >> /var/backups/logbackerupper/hashed_logs.txt
 done
 
 echo "Process complete."
